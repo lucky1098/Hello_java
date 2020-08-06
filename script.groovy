1b050fb34385dd8b811b46251b41cfbeb89c185b
lucky1098 <55136241+lucky1098@users.noreply.github.com>
2020-08-06 14:02:16 +0530

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,12 @@
+def buildApp(){
+echo "I am a build stage"
+}
+def testApp(){
+echo "I am a test stage"
+}
+def DeployApp(){
+echo "I am a deploy stage"
+echo "Deploying Version ${params.VERSION}
+}
+
+return this
